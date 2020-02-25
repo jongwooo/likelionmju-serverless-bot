543f0cd1362ec1d0aea6ffe8563a2eeede40428f
jongwooo <han980817@gmail.com>
2020-02-25 17:05:18 +0900

    Update handler for fix question replys

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
 const questions = {};
-questions["기간"] = "2020.03 - 2020.12(예정)";
-questions["대상"] =
-  "명지대학교 인문캠퍼스 재학생 / 휴학생 중 '웹 서비스'에 관심 있는 누구나 활동할 수 있습니다!\n\n1년 동안 꾸준히, 열심히, 열정적으로 활동 할 수 있는 사람을 모집합니다!!";
-questions["모집"] =
+questions["활동 기간"] = "2020.03 - 2020.12(예정)";
+questions["지원 대상"] =
+  "명지대학교 인문캠퍼스 재학생 / 휴학생 중 '웹 서비스'에 관심 있는 누구나 활동할 수 있습니다!\n\n1년 동안 꾸준히, 열정적으로 활동 할 수 있는 사람을 모집합니다!!";
+questions["모집 일정"] =
   "모집 일정이 확정되는 대로 공식 채널을 통해 알려 드리도록 하겠습니다.";
-questions["활동"] =
-  "1년 동안 나만의 서비스를 만들기 위한 창업 & 웹 프로그래밍 교육을 이수하게 됩니다!\n\n또한, 전국의 멋쟁이사자처럼 소속 학생들이 함께 하는 해커톤과 같은 이벤트에도 참여하게 됩니다.";
-questions["지원"] =
+questions["지원 방법"] =
   "지원 방법이 확정되는 대로 공식 채널을 통해 알려 드리도록 하겠습니다.";
-questions["문의"] =
+questions["활동 계획"] =
+  "1년 동안 나만의 서비스를 만들기 위한 창업 & 웹 프로그래밍 교육을 이수하게 됩니다!\n\n또한, 전국의 멋쟁이사자처럼 소속 학생들이 함께 하는 해커톤과 같은 이벤트에도 참여하게 됩니다.";
+questions["자세한 문의"] =
   "자세한 문의는 카카오톡 플러스\n친구(@likelionmyongji)를 통해 주시면 감사하겠습니다.";
 questions["멋탈!"] =
   "⊂_ヽ\n    ＼ ＼  Λ＿Λ\n         ＼( ‘ㅅ' ) 두둠칫\n            >　⌒ヽ\n           / 　    へ ＼\n         /　　/ 　   ＼＼\n        ﾚ　ノ　　   ヽ_つ\n      /　 /  두둠칫\n    / 　/|\n   (  (ヽ\n    |　|、 ＼\n    |  丿 ＼⌒)\n    |  |　　) /\n`ノ  )　Lﾉ";
@@ -52,7 +52,7 @@ exports.handler = (event, context, callback) => {
       if (messagingEvent.message && messagingEvent.message.text) {
         sendDots(messagingEvent.sender.id);
         sendTextMessage(messagingEvent.sender.id, messagingEvent.message.text);
-      }
+      } 
 
       response.statusCode = "200";
       response.body = "Success";
@@ -94,7 +94,7 @@ const sendTextMessage = (recipientId, receviedMessage) => {
 
   if (messageText === "") {
     messageText =
-      "안녕하세요, 명지대학교(서울)\n멋쟁이 사자처럼 챗봇입니다.\n기간, 대상, 모집, 활동, 지원, 문의\n중에서 원하는 키워드를 넣어서\n질문해주세요.";
+      "안녕하세요, 명지대학교(서울)\n멋쟁이 사자처럼 챗봇입니다.\n\n아래 제시된 키워드 중에서\n선택하여 질문해주세요.";
   }
 
   json = {
