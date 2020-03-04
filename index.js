80fbdcfc01d66e6ab628b5a0050b17f35de54294
jongwooo <han980817@gmail.com>
2020-03-04 17:54:03 +0900

    style: Update handler to format code

--- index.js
+++ index.js
@@ -6,123 +6,123 @@ const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 const questions = {};
 questions["활동 기간"] = "2020.03 - 2020.12(예정)";
 questions["지원 대상"] =
-  "명지대학교 인문캠퍼스 재학생 / 휴학생 중 '웹 서비스'에 관심 있는 누구나 활동할 수 있습니다!\n\n1년 동안 꾸준히, 열정적으로 활동 할 수 있는 사람을 모집합니다!!";
+	"명지대학교 인문캠퍼스 재학생 / 휴학생 중 '웹 서비스'에 관심 있는 누구나 활동할 수 있습니다!\n\n1년 동안 꾸준히, 열정적으로 활동 할 수 있는 사람을 모집합니다!!";
 questions["모집 일정"] =
-  "모집 일정이 확정되는 대로 공식 채널을 통해 알려 드리도록 하겠습니다.";
+	"모집 일정이 확정되는 대로 공식 채널을 통해 알려 드리도록 하겠습니다.";
 questions["지원 방법"] =
-  "지원 방법이 확정되는 대로 공식 채널을 통해 알려 드리도록 하겠습니다.";
+	"지원 방법이 확정되는 대로 공식 채널을 통해 알려 드리도록 하겠습니다.";
 questions["활동 계획"] =
-  "1년 동안 나만의 서비스를 만들기 위한 창업 & 웹 프로그래밍 교육을 이수하게 됩니다!\n\n또한, 전국의 멋쟁이사자처럼 소속 학생들이 함께 하는 해커톤과 같은 이벤트에도 참여하게 됩니다.";
+	"1년 동안 나만의 서비스를 만들기 위한 창업 & 웹 프로그래밍 교육을 이수하게 됩니다!\n\n또한, 전국의 멋쟁이사자처럼 소속 학생들이 함께 하는 해커톤과 같은 이벤트에도 참여하게 됩니다.";
 questions["자세한 문의"] =
-  "자세한 문의는 카카오톡 플러스\n친구(@likelionmyongji)를 통해 주시면 감사하겠습니다.";
+	"자세한 문의는 카카오톡 플러스\n친구(@likelionmyongji)를 통해 주시면 감사하겠습니다.";
 questions["Unit test"] = "Unit Testing with Jest";
 questions["멋탈!"] =
-  "⊂_ヽ\n    ＼ ＼  Λ＿Λ\n         ＼( ‘ㅅ' ) 두둠칫\n            >　⌒ヽ\n           / 　    へ ＼\n         /　　/ 　   ＼＼\n        ﾚ　ノ　　   ヽ_つ\n      /　 /  두둠칫\n    / 　/|\n   (  (ヽ\n    |　|、 ＼\n    |  丿 ＼⌒)\n    |  |　　) /\n`ノ  )　Lﾉ";
+	"⊂_ヽ\n    ＼ ＼  Λ＿Λ\n         ＼( ‘ㅅ' ) 두둠칫\n            >　⌒ヽ\n           / 　    へ ＼\n         /　　/ 　   ＼＼\n        ﾚ　ノ　　   ヽ_つ\n      /　 /  두둠칫\n    / 　/|\n   (  (ヽ\n    |　|、 ＼\n    |  丿 ＼⌒)\n    |  |　　) /\n`ノ  )　Lﾉ";
 
 exports.handler = (event, context, callback) => {
-  let method = event["httpMethod"];
-  let response = {
-    statusCode: null,
-    body: null,
-    headers: { "Content-Type": "application/json" }
-  };
-  let queryparams = event["queryStringParameters"];
+	let method = event["httpMethod"];
+	let response = {
+		statusCode: null,
+		body: null,
+		headers: { "Content-Type": "application/json" }
+	};
+	let queryparams = event["queryStringParameters"];
 
-  if (method === "GET") {
-    /* TODO: GET */
-    try {
-      if (
-        queryparams["hub.mode"] === "subscribe" &&
-        queryparams["hub.verify_token"] === VERIFY_TOKEN
-      ) {
-        response.statusCode = "200";
-        response.body = queryparams["hub.challenge"];
-      }
-    } catch (error) {
-      console.error(`Incorrect verify token: ${error}`);
-      response.statusCode = "401";
-      response.body = "Incorrect verify token";
-    }
-  } else if (method === "POST") {
-    /* TODO: POST */
-    try {
-      let bodyEvent = JSON.parse(event["body"]);
-      let messagingEvent = bodyEvent.entry[0].messaging[0];
+	if (method === "GET") {
+		/* TODO: GET */
+		try {
+			if (
+				queryparams["hub.mode"] === "subscribe" &&
+				queryparams["hub.verify_token"] === VERIFY_TOKEN
+			) {
+				response.statusCode = "200";
+				response.body = queryparams["hub.challenge"];
+			}
+		} catch (error) {
+			console.error(`Incorrect verify token: ${error}`);
+			response.statusCode = "401";
+			response.body = "Incorrect verify token";
+		}
+	} else if (method === "POST") {
+		/* TODO: POST */
+		try {
+			let bodyEvent = JSON.parse(event["body"]);
+			let messagingEvent = bodyEvent.entry[0].messaging[0];
 
-      if (messagingEvent.message && messagingEvent.message.text) {
-        sendDots(messagingEvent.sender.id);
-        sendTextMessage(messagingEvent.sender.id, messagingEvent.message.text);
-      }
+			if (messagingEvent.message && messagingEvent.message.text) {
+				sendDots(messagingEvent.sender.id);
+				sendTextMessage(messagingEvent.sender.id, messagingEvent.message.text);
+			}
 
-      response.statusCode = "200";
-      response.body = "Success";
-    } catch (error) {
-      console.error(`Internal server error: ${error}`);
-      response.statusCode = "500";
-      response.body = "Internal server error";
-    }
-  }
-  callback(null, response);
-  return response;
+			response.statusCode = "200";
+			response.body = "Success";
+		} catch (error) {
+			console.error(`Internal server error: ${error}`);
+			response.statusCode = "500";
+			response.body = "Internal server error";
+		}
+	}
+	callback(null, response);
+	return response;
 };
 
 const sendDots = recipientId => {
-  let json = {
-    recipient: { id: recipientId },
-    sender_action: "typing_on"
-  };
-  sendMessageApi(json);
+	let json = {
+		recipient: { id: recipientId },
+		sender_action: "typing_on"
+	};
+	sendMessageApi(json);
 };
 
 const sendTextMessage = (recipientId, receviedMessage) => {
-  let messageText = "";
-  let quickReplies = [];
-  let json = {};
+	let messageText = "";
+	let quickReplies = [];
+	let json = {};
 
-  for (let token in questions) {
-    if (token !== "Unit test" && token !== "멋탈!") {
-      quickReplies.push({
-        content_type: "text",
-        title: token,
-        payload: `DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_${token}`
-      });
-    }
+	for (let token in questions) {
+		if (token !== "Unit test" && token !== "멋탈!") {
+			quickReplies.push({
+				content_type: "text",
+				title: token,
+				payload: `DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_${token}`
+			});
+		}
 
-    if (receviedMessage.includes(token)) {
-      messageText += questions[token] + "\n\n";
-    }
-  }
+		if (receviedMessage.includes(token)) {
+			messageText += questions[token] + "\n\n";
+		}
+	}
 
-  if (messageText === "") {
-    messageText =
-      "안녕하세요, 명지대학교(서울)\n멋쟁이 사자처럼 챗봇입니다.\n\n아래 제시된 키워드 중에서\n선택하여 질문해주세요.";
-  }
+	if (messageText === "") {
+		messageText =
+			"안녕하세요, 명지대학교(서울)\n멋쟁이 사자처럼 챗봇입니다.\n\n아래 제시된 키워드 중에서\n선택하여 질문해주세요.";
+	}
 
-  json = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      text: messageText,
-      quick_replies: quickReplies
-    }
-  };
-  sendMessageApi(json);
+	json = {
+		recipient: {
+			id: recipientId
+		},
+		message: {
+			text: messageText,
+			quick_replies: quickReplies
+		}
+	};
+	sendMessageApi(json);
 };
 
 const sendMessageApi = messageObject => {
-  request(
-    {
-      url: `https://graph.facebook.com/v6.0/me/messages?access_token=${PAGE_ACCESS_TOKEN}`,
-      method: "POST",
-      json: messageObject
-    },
-    (error, response) => {
-      if (error) {
-        console.error(`Error sending message to user: ${error}`);
-      } else if (response.body.error) {
-        console.error(`Error sending message to user: ${response.body.error}`);
-      }
-    }
-  );
+	request(
+		{
+			url: `https://graph.facebook.com/v6.0/me/messages?access_token=${PAGE_ACCESS_TOKEN}`,
+			method: "POST",
+			json: messageObject
+		},
+		(error, response) => {
+			if (error) {
+				console.error(`Error sending message to user: ${error}`);
+			} else if (response.body.error) {
+				console.error(`Error sending message to user: ${response.body.error}`);
+			}
+		}
+	);
 };
