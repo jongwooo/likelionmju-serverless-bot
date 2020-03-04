92c1dbe8c6ad4fd58071a7c3bb658085196ad337
jongwooo <han980817@gmail.com>
2020-03-04 17:55:08 +0900

    style: Update index.test.js to format code

--- index.test.js
+++ index.test.js
@@ -4,52 +4,52 @@ const TEST_USER_ID = process.env.TEST_USER_ID;
 const CHALLENGE_TOKEN = (Math.floor(Math.random() * 1000000) + 1).toString();
 
 const getObject = {
-  httpMethod: "GET",
-  queryStringParameters: {
-    "hub.verify_token": VERIFY_TOKEN,
-    "hub.mode": "subscribe",
-    "hub.challenge": CHALLENGE_TOKEN
-  }
+	httpMethod: "GET",
+	queryStringParameters: {
+		"hub.verify_token": VERIFY_TOKEN,
+		"hub.mode": "subscribe",
+		"hub.challenge": CHALLENGE_TOKEN
+	}
 };
 
 const postObject = {
-  httpMethod: "POST",
-  body: JSON.stringify({
-    entry: [
-      {
-        messaging: [
-          {
-            message: {
-              text: "Unit test"
-            },
-            sender: {
-              id: TEST_USER_ID
-            }
-          }
-        ]
-      }
-    ]
-  })
+	httpMethod: "POST",
+	body: JSON.stringify({
+		entry: [
+			{
+				messaging: [
+					{
+						message: {
+							text: "Unit test"
+						},
+						sender: {
+							id: TEST_USER_ID
+						}
+					}
+				]
+			}
+		]
+	})
 };
 
 /* TODO: GET */
 test("GET", () => {
-  expect(
-    index.handler(getObject, null, function(isNull, res) {
-      if (!isNull) {
-        console.log(res);
-      }
-    }).body
-  ).toBe(CHALLENGE_TOKEN);
+	expect(
+		index.handler(getObject, null, function(isNull, res) {
+			if (!isNull) {
+				console.log(res);
+			}
+		}).body
+	).toBe(CHALLENGE_TOKEN);
 });
 
 /* TODO: POST */
 test("POST", () => {
-  expect(
-    index.handler(postObject, null, function(isNull, res) {
-      if (!isNull) {
-        console.log(res);
-      }
-    }).body
-  ).toBe("Success");
+	expect(
+		index.handler(postObject, null, function(isNull, res) {
+			if (!isNull) {
+				console.log(res);
+			}
+		}).body
+	).toBe("Success");
 });
