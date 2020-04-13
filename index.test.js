8b1fb80bd0237cb8dab8956652b0728ff1c2fa69
jongwooo <han980817@gmail.com>
2020-04-13 11:55:30 +0900

    test: Update index.test.js to test handler

--- index.test.js
+++ index.test.js
@@ -3,7 +3,7 @@ const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 const TEST_USER_ID = process.env.TEST_USER_ID;
 const CHALLENGE_TOKEN = (Math.floor(Math.random() * 1000000) + 1).toString();
 
-const getObject = {
+const getStatus200 = {
 	httpMethod: "GET",
 	queryStringParameters: {
 		"hub.verify_token": VERIFY_TOKEN,
@@ -12,7 +12,16 @@ const getObject = {
 	}
 };
 
-const postObject = {
+const getStatus401 = {
+	httpMethod: "GET",
+	queryStringParameters: {
+		"hub.verify_token": (Math.floor(Math.random() * 1000000) + 1).toString(),
+		"hub.mode": "subscribe",
+		"hub.challenge": CHALLENGE_TOKEN
+	}
+};
+
+const postStatus200 = {
 	httpMethod: "POST",
 	body: JSON.stringify({
 		entry: [
@@ -32,10 +41,30 @@ const postObject = {
 	})
 };
 
+const postStatus500 = {
+	httpMethod: "POST",
+	body: JSON.stringify({
+		entry: [
+			{
+				messaging: [
+					{
+						message: {
+							// text: "Unit test"
+						},
+						sender: {
+							id: TEST_USER_ID
+						}
+					}
+				]
+			}
+		]
+	})
+};
+
 /* TODO: GET */
-test("GET", () => {
+test("GET::status 200", () => {
 	expect(
-		index.handler(getObject, null, function(isNull, res) {
+		index.handler(getStatus200, null, function(isNull, res) {
 			if (!isNull) {
 				console.log(res);
 			}
@@ -43,13 +72,33 @@ test("GET", () => {
 	).toBe(CHALLENGE_TOKEN);
 });
 
-/* TODO: POST */
-test("POST", () => {
+test("GET::status 401", () => {
 	expect(
-		index.handler(postObject, null, function(isNull, res) {
+		index.handler(getStatus401, null, function(isNull, res) {
+			if (!isNull) {
+				console.log(res);
+			}
+		}).body
+	).toBe("Incorrect verify token");
+});
+
+/* TODO: POST */
+test("POST:: status 200", () => {
+	expect(
+		index.handler(postStatus200, null, function(isNull, res) {
 			if (!isNull) {
 				console.log(res);
 			}
 		}).body
 	).toBe("Success");
+});
+
+test("POST:: status 500", () => {
+	expect(
+		index.handler(postStatus500, null, function(isNull, res) {
+			if (!isNull) {
+				console.log(res);
+			}
+		}).body
+	).toBe("Internal server error");
 });
