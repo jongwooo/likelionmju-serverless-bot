d514cb3b422ddabc6ee7f56deab85e9fd580558d
jongwooo <han980817@gmail.com>
2020-05-08 02:02:48 +0900

    style: Update index.test.js to format code

--- index.test.js
+++ index.test.js
@@ -4,6 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
 "use strict";
 const index = require("./index.js");
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
