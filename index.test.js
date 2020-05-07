f67f488a4d6c4bed0ae36a09b7e543c80e0168b7
jongwooo <han980817@gmail.com>
2020-05-08 02:02:14 +0900

    test: Update index.test.js to add copyright

--- index.test.js
+++ index.test.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2020 Jongwoo Han
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+"use strict";
 const index = require("./index.js");
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 const TEST_USER_ID = process.env.TEST_USER_ID;
