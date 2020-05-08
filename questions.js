585755f55e706c70b8f47d5ca74f3a10976f71f0
jongwooo <han980817@gmail.com>
2020-05-08 11:15:00 +0900

    feat: Update questions.js to add copyright

--- questions.js
+++ questions.js
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2020 Jongwoo Han
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+"use strict";
 const questions = {};
 questions["활동 기간"] = "2020.04 - 2020.12";
 questions["지원 대상"] =
