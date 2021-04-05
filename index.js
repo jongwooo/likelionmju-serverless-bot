0c6ebf3438fa472585422e87d3f239560bd69d8b
jongwooo <han980817@gmail.com>
2021-04-05 19:54:29 +0900

    feat: Update test message reply case in handler

--- index.js
+++ index.js
@@ -153,7 +153,7 @@ const sendTextMessage = (recipientId, receivedMessage) => {
 	}
 
 	if (receivedMessage.includes("Unit test")) {
-		messageText = `[Unit test]\n\n-Sender ID: ${recipientId}\n-Facebook API v ${FACEBOOK_API_VERSION}\n -Copyright (c) 2020 Jongwoo Han`;
+		messageText = `[Unit test]\n\n-Sender ID: ${recipientId}\n-Facebook API v ${FACEBOOK_API_VERSION}\n`;
 	}
 
 	if (messageText === "") {
