9a8862655d3f0ffb6f06001ed99a643f7f396b16
jongwooo <han980817@gmail.com>
2021-11-14 16:39:13 +0000

    test: Modify the asynchronous call of the test code

--- index.test.js
+++ index.test.js
@@ -230,14 +230,9 @@ const unitTest = (method, statusCode, caseNum, caseType, toBeReturn) => {
 		testName = `${testName} Case ${caseNum}`;
 	}
 
-	it(testName, () => {
-		expect(
-			index.handler(caseType, null, (isNotNull, response) => {
-				if (isNotNull) {
-					console.error(response);
-				}
-			}).body
-		).toBe(toBeReturn);
+	it(testName, async () => {
+		const props = await index.handler(caseType);
+		expect(props.body).toBe(toBeReturn);
 	});
 };
 
